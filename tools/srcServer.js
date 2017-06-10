70338967d2984a08bb63d5bd64ad45a9534612ba
Khaled Ali <alikkhaledali@gmail.com>
2017-06-10 18:18:46 -0400

    Env Setup completed

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -7,8 +7,8 @@ import open from 'open';
 /* eslint-disable no-console */
 
 const port = 3000;
-const app = express();
 const compiler = webpack(config);
+const app = express();
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
